f282cb85b027ff635f270025ae56e3f7894e10b8
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2022-04-27 22:57:51 +0300

    about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
           </article>
         </div>
         <p>asd</p>
-        <a href="#contact" className='btn'>Let's Talk</a>
+        {/* <a href="#contact" className='btn btn-primary'>Let's Talk</a> */}
       </div>
     </div>
   </section>
