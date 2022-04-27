83b78382c25116a9cc500756bb82b3df6a74056f
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2022-04-27 22:49:05 +0300

    about me section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
-import Portrait from '../../assets/Slush_portrait.jpg'
+import Portrait from '../../assets/slush-purple2.jpg'
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5 className="text-light">Hello I'm</h5>
         <h1 className="text-dark">Otso Weckström</h1>
         <h5 className="text-light">Software Engineering Student</h5>
         <h5 className="text-light">Developer / Consultant</h5>
-
-        <CTA />
-
+        
         <a href="#contact" className="scroll__down">
           Scroll Down
         </a>
         <div className="me">
           <img src={Portrait} alt="me" />
         </div>
+
+        <CTA />
+
 
         <HeaderSocials />
       </div>
