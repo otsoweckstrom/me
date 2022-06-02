a8ea5c6a6b93768a63ceb0360982db77fb56dc91
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2022-06-03 00:25:41 +0300

    add experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,14 +6,103 @@ const Experience = () => {
         <section id="experience">
             <h5>So what can I do?</h5>
             <h2>Skills</h2>
-            <div className="container experience__container">
-            <div><h1>hello</h1></div>
-            <div><h1>hello</h1></div>
-            <div><h1>hello</h1></div>
+            <div className="experience__container">
+                <div className="experience__column">
+                    <h1 className="experience__title">Languages</h1>
+                    <div className="list__container">
+                        <ul class="a">
+                            <li>React</li>
+                            <li>React Native</li>
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                            <li>C#</li>
+                            <li>C</li>
+                        </ul>
+                        <ul class="b">
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Decent</li>
+                            <li>Decent</li>
+                            <li>Few Unity Projects</li>
+                            <li>Basics</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="experience__column">
+                    <h1 className="experience__title">Technologies</h1>
+                    <div className="list__container">
+                        <ul class="a">
+                            <li>Node.js</li>
+                            <li>s</li>
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                            <li>C#</li>
+                            <li>C</li>
+                        </ul>
+                        <ul class="b">
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Decent</li>
+                            <li>Decent</li>
+                            <li>Few Unity Projects</li>
+                            <li>Basics</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="experience__column">
+                    <h1 className="experience__title">Other</h1>
+                    <div className="list__container">
+                        <ul class="a">
+                            <li>Github</li>
+                            <li>s</li>
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                            <li>C#</li>
+                            <li>C</li>
+                        </ul>
+                        <ul class="b">
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Good</li>
+                            <li>Decent</li>
+                            <li>Decent</li>
+                            <li>Few Unity Projects</li>
+                            <li>Basics</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="experience__column">
+                    <h1 className="experience__title">Gaming</h1>
+                    <div className="list__container">
+                        <ul class="a">
+                            <li>League Of Legends</li>
+                            <li>CS:GO</li>
+                            <li>Hearthstone</li>
+                            <li>Clash Royale</li>
+                        </ul>
+                        <ul class="b">
+                            <li>Diamond</li>
+                            <li>Global Elite</li>
+                            <li>Legend</li>
+                            <li>Legendary Arena</li>
+                        </ul>
+                    </div>
+                    <p className="experience__bottom">
+                        Yeah I'm a bit of a gamer 😎
+                    </p>
+                </div>
             </div>
-      
-         
-        
         </section>
     )
 }
